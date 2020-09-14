88a08a4e313ae2f31982777f8fa58d31973fd71f
devpalz <70041498+devpalz@users.noreply.github.com>
2020-09-14 21:47:38 +0100

    Initial commit

---
+++ bin/cdk-cfn-response-example.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import 'source-map-support/register';
+import { CdkCfnResponseExample } from '../lib/cdk-cdn-response-example-stack';
+
+const app = new cdk.App();
+new CdkCfnResponseExample(app, 'Cfn-Response-Example-Stack');
